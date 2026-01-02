2f14982d320b65ca1f7474a56cbafb0a98c4a6eb
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-01-02 10:51:00 +0530

    express completed

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import http from "http";
 import dotenv from "dotenv";
+import express from "express";
+import { log } from "console";
 
+
+const app = express();
 
 dotenv.config();
 
 const port=process.env.PORT || 5000;
 
 
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.status(200).send(`<h1 style="text-align:center;">Welcome to Backend </h1>`);
+})
 
 
-
-http.createServer((req,res)=>{
+/* http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/plain"});
     res.write("Hello all welcome to the node backend");
     res.end();
 }).listen(port,() =>{
     console.log(`server is started and running on the port${port}`);
+}) */
+
+app.listen(port,()=>{
+    console.log("server started");
 })
