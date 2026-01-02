398d11cfba7dfb081b715aadff1978be3606a1a9
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-01-02 10:29:14 +0530

    port changed and nodemon installed

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import http from "http";
+import dotenv from "dotenv";
 
 
+dotenv.config();
+
+const port=process.env.PORT || 5000;
 
 
-const port=5000;
 
 
 
 http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/plain"});
-    res.write("Hello welcome to the backend");
+    res.write("Hello all welcome to the node backend");
     res.end();
 }).listen(port,() =>{
     console.log(`server is started and running on the port${port}`);
